bf05e186540bd0e84ee519bfe74ce28b7b7c4a66
Rafalp190 <leo13361@uvg.edu.gt>
2018-07-16 12:53:58 -0600

    completed xls_to_csv data cleanup

--- DataCleanup/xls_to_csv.R
+++ DataCleanup/xls_to_csv.R
@@ -3,31 +3,31 @@ require("readxl")
 # Script to convert XLS files to csv for standarization
 
 ## Deceased and Injured
-fl17 <- read_excel("./Datos_brutos/17FL.xslx")
-write.csv("./Data/17FL.csv")
+fl17 <- read_excel("Datos_brutos/17FL.xlsx")
+write.csv(fl17, "Data/17FL.csv")
 
-fl16 <- read_excel("Datos_brutos/16FL.xslx")
-write.csv("Data/16FL.csv")
+fl16 <- read_excel("Datos_brutos/16FL.xlsx")
+write.csv(fl16,"Data/16FL.csv")
 
-fl15 <- read_excel("Datos_brutos/15FL.xslx")
-write.csv("Data/15FL.csv")
+fl15 <- read_excel("Datos_brutos/15FL.xlsx")
+write.csv(fl15, "Data/15FL.csv")
 
 ## Involved Vehicles
-vi17 <- read_excel("Datos_brutos/17VI.xslx")
-write.csv("Data/17VI.csv")
+vi17 <- read_excel("Datos_brutos/17VI.xlsx")
+write.csv(vi17,"Data/17VI.csv")
 
-vi16 <- read_excel("Datos_brutos/16VI.xslx")
-write.csv("Data/16VI.csv")
+vi16 <- read_excel("Datos_brutos/16VI.xlsx")
+write.csv(vi16,"Data/16VI.csv")
 
-vi15 <- read_excel("Datos_brutos/15VI.xslx")
-write.csv("Data/15VI.csv")
+vi15 <- read_excel("Datos_brutos/15VI.xlsx")
+write.csv(vi15, "Data/15VI.csv")
 
 ## Transit Events
-ht17 <- read_excel("Datos_brutos/17HT.xslx")
-write.csv("Data/17HT.csv")
+ht17 <- read_excel("Datos_brutos/17HT.xlsx")
+write.csv(ht17, "Data/17HT.csv")
 
-ht16 <- read_excel("Datos_brutos/16HT.xslx")
-write.csv("Data/16HT.csv")
+ht16 <- read_excel("Datos_brutos/16HT.xlsx")
+write.csv(ht16, "Data/16HT.csv")
 
-ht15 <- read_excel("Datos_brutos/15HT.xslx")
-write.csv("Data/15HT.csv")
+ht15 <- read_excel("Datos_brutos/15HT.xlsx")
+write.csv(ht15, "Data/15HT.csv")
